0cda2c17a1ffa0e27c7fbc637e3b6e78a7b37061
sik2 <bickwang123@gmail.com>
2025-03-11 11:08:25 +0900

    init

---
+++ src/main/java/com/ll/sbrdsreplication/SbRdsReplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbrdsreplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRdsReplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbRdsReplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbrdsreplication/SbRdsReplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbrdsreplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRdsReplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
